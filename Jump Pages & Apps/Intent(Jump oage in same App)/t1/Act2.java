133b33074662049f25b3e112ca51d1e8abc247df
sajithlakshan <57319180+sajithlakshan@users.noreply.github.com>
2022-10-18 23:21:49 +0530

    Add files via upload

---
+++ Jump Pages & Apps/Intent(Jump oage in same App)/t1/Act2.java
@@ -0,0 +1,28 @@
+package com.example.t1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class Act2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_act2);
+        Button btn = findViewById(R.id.bu2);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent I = new Intent(getApplicationContext(),MainActivity.class);
+                startActivity(I);
+
+
+            }
+        });
+    }
+}
